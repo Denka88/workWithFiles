d0df449901862fc3365beedd8e7ba5047bdb9822
Denka88 <animedenisik.com@mail.ru>
2024-11-05 21:41:52 +0300

    Создан класс Task, начато изучение работы с каталогами

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,22 @@
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
 
+        File dir = new File("G:/JavaLearn/workWithFiles/NewDir");
 
-
+        boolean created = dir.mkdir();
+        if (created) {
+            System.out.println("Folder has been created");
+        }
+        // Переименуем каталог
+        File newDir = new File("G:/JavaLearn/workWithFiles/NewDir2");
+        dir.renameTo(newDir);
+        // Удалим каталог
+        boolean deleted = newDir.delete();
+        if (deleted) {
+            System.out.println("Folder has been deleted");
+        }
+        
     }
 }
