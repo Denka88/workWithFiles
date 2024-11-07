e961f25e1163904b1711ad9f0710d120c3813aff
Denka88 <shadowfightdenka88@gmail.com>
2024-11-07 16:27:52 +0300

    Реализован метод создания/открытия файла

--- src/UserManager.java
+++ src/UserManager.java
@@ -1,7 +1,34 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+
 public class UserManager {
     public static void main(String[] args) {
 
+        Scanner scan = new Scanner(System.in);
 
+        System.out.print("Введите имя желаемого файла: ");
+
+        String fileName = scan.nextLine();
+        createFile(fileName);
 
     }
+
+    public static void createFile(String fileName) {
+
+        File newFile = new File(fileName);
+
+        if(newFile.exists()){
+            System.out.println("Файл с таким именем существует");
+        }else{
+            try {
+                newFile.createNewFile();
+                System.out.println("Файл успешно создан");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
 }
